ea4a386819185b5f4f2b0b47f5365777e13c77e2
John Vilk <jvilk@cs.umass.edu>
2015-10-20 16:05:49 -0400

    Progress on v0.1. Untested, but theoretically should work.

---
+++ ts/index.ts
@@ -0,0 +1,38 @@
+import transformTools = require('browserify-transform-tools');
+import FalafelNode = transformTools.FalafelNode;
+import {IUndebugifyOptions} from './interfaces';
+
+interface FalafelExpressionStatement extends FalafelNode, ESTree.ExpressionStatement {}
+interface FalafelCallExpression extends FalafelNode, ESTree.CallExpression {}
+
+function isFalafelExpressionStatement(node: FalafelNode): node is FalafelExpressionStatement {
+  return node && node.type === 'ExpressionStatement';
+}
+
+function isFalafelCallExpression(node: ESTree.Expression): node is FalafelCallExpression {
+  return node && node.type === 'CallExpression';
+}
+
+function isIdentifier(node: ESTree.Expression): node is ESTree.Identifier {
+  return node && node.type === 'Identifier';
+}
+
+
+export = transformTools.makeFalafelTransform<IUndebugifyOptions>("array-fnify", {
+ jsFilesOnly: true
+}, function (node, transformOptions, done): void {
+  // Remove CallExpressions representing:
+  // aFunctionInRemove(<any>)
+  var toRemove = transformOptions.configData.config.remove;
+  if (isFalafelExpressionStatement(node)) {
+    var expression = node.expression;
+    if (isFalafelCallExpression(expression)) {
+      var callee = expression.callee;
+      if (isIdentifier(callee) && toRemove.indexOf(callee.name) > -1) {
+        // Remove node.
+        node.update('');
+      }
+    }
+  }
+  done();
+});
---
+++ ts/interfaces.ts
@@ -0,0 +1,7 @@
+/**
+ * Options to the debugify transform.
+ */
+export interface IUndebugifyOptions {
+  // List of functions to remove from the program.
+  remove: string[];
+}
